8a829cfcf14eaa504881989a3074583b022c8870
Yunus Ozmen <yuozmen1@gmail.com>
2024-05-05 22:15:24 +0200

    Initial commit

---
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package com.trycloud.pages;
+
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id="user")
+    public WebElement userName;
+
+
+
+    @FindBy(id="password")
+    public WebElement password;
+
+    @FindBy(id = "_submit-form")
+    public WebElement submit;
+
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+        // verification that we logged
+    }
+
+}
