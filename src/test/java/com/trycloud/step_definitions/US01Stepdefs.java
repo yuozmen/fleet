c6c4ca59e853c1d7689f222072dd4e0dc9a578cf
shehrzd <staystrong0729@gmail.com>
2024-05-08 10:21:13 +0200

    size of string done

--- src/test/java/com/trycloud/step_definitions/US01Stepdefs.java
+++ src/test/java/com/trycloud/step_definitions/US01Stepdefs.java
@@ -2,8 +2,11 @@ package com.trycloud.step_definitions;
 
 import com.trycloud.pages.BasePage;
 import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
@@ -13,8 +16,18 @@ public class US01Stepdefs {
     @Then("user sees below module")
     public void userSeesBelowModule(List<String> expectedOptions) {
 
-        List<String>actualOptions = BrowserUtils.dropdownOptions_as_STRING(BasePage.menuOptions);
+        List<WebElement> listOfModule = Driver.getDriver().findElements(By.xpath("//ul[@class='nav-multilevel main-menu']/li"));
+
+        System.out.println("listOfModule.size() = " + listOfModule.size());
+
+       // List<String>actualOptions = BrowserUtils.getElementsText(listOfModule);
+
+        //Assert.assertEquals(expectedOptions,actualOptions);
+
+       /* List<String>actualOptions = BrowserUtils.dropdownOptions_as_STRING(BasePage.menuOptions);
 
         Assert.assertEquals(actualOptions, expectedOptions);
+
+        */
     }
 }
